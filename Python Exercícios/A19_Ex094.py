d867b8795e9879baff92d7412878ace5f016ba60
Lucas Eduardo Borba <122682751+lucaseeeduardo@users.noreply.github.com>
2023-02-19 15:22:42 -0300

    Update A19_Ex094.py
    Código, agora, finalizado, com a realização de todas as tasks previstas para o exercício.

--- Python Exercícios/A19_Ex094.py
+++ Python Exercícios/A19_Ex094.py
@@ -2,6 +2,7 @@
 cadastro = dict()
 banco = list()
 validacao = 's'
+listamulheres = list()
 while validacao not in 'nN':
     cadastro['nome'] = str(input('Nome: '))
     cadastro['sexo'] = str(input('Sexo [M/F]: '))
@@ -10,11 +11,27 @@ while validacao not in 'nN':
         cadastro['sexo'] = str(input('Sexo [M/F]: '))
     cadastro['idade'] = int(input('Idade: '))
     banco.append(cadastro.copy())
+    if cadastro['sexo'] in 'Ff':
+        listamulheres.append(cadastro.copy())
     validacao = str(input('Quer continuar? [S/N] '))
     while validacao not in 'sS' and validacao not in 'Nn':
         print('ERRO! Responda apenas S ou N.')
         validacao = str(input('Quer continuar? [S/N] '))
 print(f'A) Ao todo temos {len(banco)} pessoas cadastradas.')
-print(f'B) A média de idade é de {} anos')
-print(f'C) Mulheres cadastradas')
+isoma = 0
+# banco = [{'nome': Lucas, 'sexo': m, 'idade': 10}, {}]
+for c in range(0, len(banco)):
+    isoma = isoma + banco[c]['idade']
+
+print(f'B) A média de idade é de {isoma/len(banco)} anos')
+print(f'C) As mulheres cadastradas foram: ', end='')
+for mulheres in range(0, len(listamulheres)):
+    print(listamulheres[mulheres]['nome'], end=' ')
+print()
 print(f'D) Lista de pessoas acima da média: ')
+# banco = [{'nome': Lucas, 'sexo': m, 'idade': 10}]
+for pessoa in range(0, len(banco)):
+    if banco[pessoa]['idade'] > isoma/len(banco):
+        for k, v in banco[pessoa].items():
+            print(f'{k} = {v};', end=' ')
+        print()
