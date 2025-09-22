d747bcdb53bbdef880de911b93e48a5c69e12a50
Lucas Eduardo <lucas.eduardo@tunesoft.com.br>
2025-09-22 19:56:49 -0300

    organizando pastas

--- Programação Para Dispositivos Móveis/GameEntryComparator.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class GameEntryComparator implements Comparator<GameEntry> {
-    
-     @Override
-    public int compare(GameEntry entrada1, GameEntry entrada2) {
-        return Integer.compare(entrada1.getScore(), entrada2.getScore());
-    }
-}
